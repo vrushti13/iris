824d6f0c0d72aca33df0d30e8cfd93d758471613
vrushti13 <87222801+vrushti13@users.noreply.github.com>
2021-07-12 22:23:47 +0530

    Add files via upload

---
+++ mycap_assignment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    float a, b;
+    float c;
+    cout<<"hello user";
+    cout<< endl;
+    cout<<"enter a number: ";
+    cin>> a ;
+    cout<< endl;
+    cout<<"enter another number: ";
+    cin>> b;
+    cout<<endl;
+    c = a * b; 
+    cout<< endl;
+    cout<<"the product is "<< c;
+}
